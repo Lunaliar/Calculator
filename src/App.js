a169e98ceb92fdba04670c1aaece5c9c46d6a80b
Sav <98275089+Lunaliar@users.noreply.github.com>
2022-10-19 12:27:43 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./style.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const buttonSymbols = [
+		"Clear",
+		"←",
+		"/",
+		"7",
+		"8",
+		"9",
+		"*",
+		"4",
+		"5",
+		"6",
+		"-",
+		"1",
+		"2",
+		"3",
+		"+",
+		".",
+		"0",
+		"=",
+	];
+	const [display, setDisplay] = useState("");
+	const [result, setResult] = useState(false);
+
+	const handleClick = (e) => {
+		console.log(`display is: ${display}`);
+		console.log(display.charAt(display.length - 1));
+		if (display === "Error") {
+			setDisplay("");
+		} else {
+			switch (e.target.innerText) {
+				case "←": {
+					setDisplay((display) => display.slice(0, -1));
+					break;
+				}
+				case "/":
+				case "+":
+				case "*":
+				case "-": {
+					console.log(display.charAt(display.length - 1));
+					if (
+						display.charAt(display.length - 1) === "" ||
+						display.charAt(display.length - 1) === "*" ||
+						display.charAt(display.length - 1) === "-" ||
+						display.charAt(display.length - 1) === "+" ||
+						display.charAt(display.length - 1) === "/"
+					) {
+						console.log("incorrect");
+					} else {
+						setDisplay((currDisplay) => (currDisplay += e.target.innerText));
+					}
+					break;
+				}
+				case "Clear": {
+					setDisplay("");
+					break;
+				}
+				case "=": {
+					if (display === "") {
+						break;
+					} else {
+						try {
+							let evalDisplay = String(eval(display));
+							setDisplay(evalDisplay);
+						} catch (e) {
+							setDisplay("Error");
+							console.log(e);
+						}
+					}
+					break;
+				}
+				case ".": {
+					if (display === "") {
+						setDisplay("0");
+					}
+					if (display.charAt(display.length - 1) === ".") {
+						break;
+					}
+				}
+				default: {
+					setDisplay((currDisplay) => (currDisplay += e.target.innerText));
+					break;
+				}
+			}
+		}
+	};
+
+	return (
+		<div className="container">
+			<div
+				className="display"
+				placeholder="."
+			>
+				{display}
+			</div>
+			<div className="buttons">
+				{buttonSymbols.map((b) => {
+					return (
+						<div
+							onClick={(e) => handleClick(e)}
+							className={`button ${b} ${b === "=" && "equals"}`}
+							key={b}
+						>
+							{b}
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
